e8138887811035118119c5a818b9f9acf91dc85a
carole macharia <carolemacharia@gmail.com>
2020-10-10 19:33:31 +0400

    first commit

---
+++ src/SearchButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function SearchButton() {
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search"
+      />
+     
+    </div>
+  );
+}
+export default SearchButton;
